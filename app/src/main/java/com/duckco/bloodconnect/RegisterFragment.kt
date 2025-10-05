97f08729e2e25a95d83139761ab2718676218ac3
avitheduck <abhijitsah567@gmail.com>
2025-10-05 23:44:51 +0530

    First commit

---
+++ app/src/main/java/com/duckco/bloodconnect/RegisterFragment.kt
@@ -0,0 +1,28 @@
+package com.duckco.bloodconnect
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.registerButton).setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_dashboardFragment)
+        }
+    }
+}
